e991fd65f6779f5e7b79da1fdb98d2b96536c56c
Corbin Page <corbin.page@gmail.com>
2016-03-13 22:51:41 -0400

    Started copying an app

--- capybara.rb
+++ capybara.rb
@@ -12,13 +12,19 @@ module MyCapybaraTest
 	class Test
 		include Capybara::DSL
 
+		def sign_in
+			fill_in 'un', :with => 'cpage'
+			fill_in 'pw', :with => 'testtest'
+			find('.button_box .btn.primary').click
+		end
+
 		def log_objects(app_name)
+
 			visit('/design')
 
 			sign_in
-			page.has_css?("table.GN55TI2M1")
+			page.has_css?("div.aui-DataGrid-Table table")
 			click_link app_name
-			page.has_css?("body div.aui-DataGrid-Table.GN55TI2LUC")
 
 			CSV.open('./app-objects.csv', 'w+') do |csv|
 				csv << ["Name","Type"]
@@ -28,19 +34,15 @@ module MyCapybaraTest
 
 		end
 
-		def sign_in
-			fill_in 'un', :with => 'cpage'
-			fill_in 'pw', :with => ''
-			find('.button_box .btn.primary').click
-		end
-
 		def save_objects(csv)
-			all('body div.aui-DataGrid-Table.GN55TI2LUC tbody tr').each_with_index do |tr, i|
+			puts page.has_css?("body div.aui-DataGrid-Table table tbody")
+			page.has_css?("body div.aui-DataGrid-Table table tbody")
+			all('body div.aui-DataGrid-Table table tbody tr').each_with_index do |tr, i|
 				puts i+1
-				puts first('body div.aui-DataGrid-Table.GN55TI2LUC tr:nth-child('+(i+1).to_s+') td:nth-child(3) a').text
+				puts first('body div.aui-DataGrid-Table table tbody tr:nth-child('+(i+1).to_s+') td:nth-child(3) a').text
 
-				name = first('body div.aui-DataGrid-Table.GN55TI2LUC tr:nth-child('+(i+1).to_s+') td:nth-child(3) a').text
-				type = first('body div.aui-DataGrid-Table.GN55TI2LUC tr:nth-child('+(i+1).to_s+') td:nth-child(2) img')['alt']
+				name = first('body div.aui-DataGrid-Table table tbody tr:nth-child('+(i+1).to_s+') td:nth-child(3) a').text
+				type = first('body div.aui-DataGrid-Table table tbody tr:nth-child('+(i+1).to_s+') td:nth-child(2) img')['alt']
 
 				object_data = [
 					name,
@@ -49,9 +51,9 @@ module MyCapybaraTest
 				csv << object_data
 			end
 
-			next_page_button = first('body div.GN55TI2KX.GN55TI2MX div.aui-DataGridPager-Container > table > tbody > tr > td:nth-child(4) > img')
+			next_page_button = first('body div.aui-DataGridPager-Container table > tbody > tr > td:nth-child(4) > img')
 			if(next_page_button && next_page_button['aria-disabled'] == 'false')
-				current_page_text = first('body div.GN55TI2KX.GN55TI2MX div.aui-DataGridPager-Container > table > tbody > tr > td.GN55TI2H2B > div').text
+				current_page_text = first('body div.aui-DataGridPager-Container table > tbody > tr > td:nth-child(3) > div').text
 
 				next_page_button.click
 				page.has_no_text?(current_page_text)
@@ -59,12 +61,99 @@ module MyCapybaraTest
 			end
 
 		end
+
+		def create_new_app(app_name)
+			click_button "New Application"
+			page.has_text?("Create New Application")
+
+			fill_in 'gwt-uid-100', :with => app_name
+			find_button('Create').click
+			page.has_css?("body div.aui-DataGrid-Table table")
+
+		end
+
+		def create_copied_app(csv, old_prefix, new_prefix)
+			visit('/design')
+
+			sign_in
+			page.has_css?("div.aui-DataGrid-Table table")
+
+			create_new_app(new_prefix)
+
+			inputs = CSV.read(csv)
+			inputs.reverse.each_with_index do |data,i|
+
+				copy_object(data)
+
+			end
+
+		end
+
+		def copy_object(data)
+			click_link 'New'
+
+			if page.has_text? data[1]
+				click_link data[1]
+				page.has_no_text?("Create from Scratch")
+				click_button 'Cancel'
+				puts "Found: " + data[1]
+			else
+				puts "Not found: " + data[1]
+			end
+
+
+			# Data Type
+			# Data Store
+			# Record Type
+
+			# Process Model
+			# Process Report
+
+			# Interface
+			# Report
+			# Site
+
+			# Expression Rule
+			# Constant
+			# Query Rule
+			# Web API
+
+			# Document
+			# Group
+			# Group Type
+			# Feed
+			# Folder
+
+		end
+
+# inputs = inputs[1...-1]
+
+# CSV.open('./input-sites.csv', 'w+') do |csv|
+# 	csv << ["Id","Links","Center","City","State"]
+
+		# end
+
+		# def create_app(new_prefix)
+		# 	find('body div.GKACFQNDFOB.GKACFQNDGOB div.content_toolbar button:nth-child(1)').click
+		# 	# find('New Application').click
+		# 	page.has_text?('Create New Application')
+
+		# 	fill_in 'gwt-uid-100', :with => new_prefix
+
+		# 	find('Create').click
+		# 	page.has_text?('Create New Application')
+
+		# 	return new_prefix
+		# end
+
 	end
 end
 
 t = MyCapybaraTest::Test.new
 # t.log_objects('NAV_SA Starter Application')
-t.log_objects('NAV_TM Task Metric Utilities')
+# t.log_objects('NAV_TM Task Metric Utilities')
+t.create_copied_app('app-objects.csv', 'NAV_TM', 'ZZ_TEST')
+
 
 
 
